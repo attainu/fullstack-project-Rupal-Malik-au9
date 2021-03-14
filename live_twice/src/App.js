57ce120eb88eebb2e02c5f32f15ec4545cf91124
Rupal Malik <rupalmalik620@gmail.com>
2021-03-14 09:36:27 +0530

    updating1

--- live_twice/src/App.js
+++ live_twice/src/App.js
@@ -1,12 +1,17 @@
 import Header from './components/header';
 import Navbar from './components/navbar';
+
+import Categories_section from './components/categories_section';
 import './App.css';
+// import Register from './pages/Register';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
+      <Categories_section />
+      {/* <Register /> */}
     </div>
   );
 }
