bb772fb0848298667d8592077769a9fdb34e3e3f
Rupal Malik <rupalmalik620@gmail.com>
2021-03-25 10:16:36 +0530

    schema for travel

---
+++ live_twice/backend/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGOURI:
+      "mongodb+srv://rupal:piyush88@cluster0.77bw0.mongodb.net/live_twice?retryWrites=true&w=majority",
+    JWT_SECRET: "aasasadsdsdsdswewrr",
+  };
